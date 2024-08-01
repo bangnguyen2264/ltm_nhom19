015120edf9ff09eae125e45ae3c24af11e8a5e17
bangnguyen2264 <2251120335@ut.edu.vn>
2024-08-01 10:50:25 +0700

    first commit

---
+++ ChatApp/PrivateChat.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ChatApp
+{
+    partial class PrivateChat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listTextMessages = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.ipConnect = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listTextMessages
+            // 
+            this.listTextMessages.Location = new System.Drawing.Point(17, 42);
+            this.listTextMessages.Name = "listTextMessages";
+            this.listTextMessages.Size = new System.Drawing.Size(453, 342);
+            this.listTextMessages.TabIndex = 6;
+            this.listTextMessages.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(354, 390);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(116, 51);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(17, 390);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(331, 51);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(402, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(68, 27);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ipConnect
+            // 
+            this.ipConnect.Location = new System.Drawing.Point(334, 14);
+            this.ipConnect.Name = "ipConnect";
+            this.ipConnect.Size = new System.Drawing.Size(62, 22);
+            this.ipConnect.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.label1.Location = new System.Drawing.Point(261, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Client Port";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F);
+            this.label2.Location = new System.Drawing.Point(22, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Your Port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PrivateChat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ipConnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.listTextMessages);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "PrivateChat";
+            this.Text = "PrivateChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox listTextMessages;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox ipConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ ChatApp/PrivateChat.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ChatApp
+{
+    public partial class PrivateChat : Form
+    {
+        private TcpClient client;
+        private TcpListener listener;
+        private NetworkStream stream;
+        private Thread listenerThread;
+
+
+        public PrivateChat()
+        {
+            InitializeComponent();
+            ConnectToServer();
+        }
+
+        void ConnectToServer()
+        {
+            ip = new IPEndPoint(IPAddress.Any, 5000);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Enable address reuse
+            server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+            try
+            {
+                server.Bind(ip);
+            }
+            catch (SocketException ex)
+            {
+                Trace.WriteLine($"Error binding socket: {ex.Message}");
+                MessageBox.Show($"Error binding socket: {ex.Message}", "Socket Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Thread listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Server error: {ex.Message}", "Server Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ResetServer();
+                }
+            })
+            {
+                IsBackground = true
+            };
+            listen.Start();
+        }
+
+        void ResetServer()
+        {
+            ip = new IPEndPoint(IPAddress.Any, 5000);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+        }
+
+        void ConnectToClient()
+        {
+            ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), int.Parse(ipConnect.Text));
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                client.Connect(ip);
+                Thread receive = new Thread(Receive)
+                {
+                    IsBackground = true
+                };
+                receive.Start(client);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unable to connect to client: {ex.Message}", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void Close()
+        {
+            server?.Close();
+            client?.Close();
+        }
+
+        void Send()
+        {
+            if (!string.IsNullOrEmpty(txtMessage.Text))
+            {
+                client.Send(Serialize(txtMessage.Text));
+            }
+        }
+
+        void Receive(object obj)
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+                    string message = (string)Deserialize(data);
+                    AddMessage(message);
+                }
+            }
+            catch
+            {
+                client?.Close();
+            }
+        }
+
+        void AddMessage(string message)
+        {
+            listTextMessages.Invoke(new Action(() => listTextMessages.Text += message + Environment.NewLine));
+        }
+
+        byte[] Serialize(object obj)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        object Deserialize(byte[] data)
+        {
+            using (MemoryStream stream = new MemoryStream(data))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return formatter.Deserialize(stream);
+            }
+        }
+
+        private void PrivateChat_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectToClient();
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
