9f6885cfd3f56b7dca126147a4b41c1008b47cea
bangnguyen2264 <2251120335@ut.edu.vn>
2024-08-06 13:01:40 +0700

    feat: add server run

---
+++ Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+//Tín push code Server
+class Server
+{
+
+    static void Main(string[] args)
+    {
+        
+    }
+
+}
