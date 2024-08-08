9c29fb38feef9791b30ba253f9d1ba39745edf50
Doyle252 <thainguyen2522004@gmail.com>
2024-08-08 15:36:02 +0700

    backgroundworker1

--- ChatApp/PrivateChat.cs
+++ ChatApp/PrivateChat.cs
@@ -11,20 +11,48 @@ namespace ChatApp
 {
     public partial class PrivateChat : Form
     {
-
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public string TextToSend;
+        public bool isConnected = false;
 
         public PrivateChat()
         {
             InitializeComponent();
+            yPort.Text = User.PrivatePort.ToString();
         }
 
         private void btnSend_Click(object sender, EventArgs e)
         {
+            if (txtMessage.Text != "")
+            {
+                TextToSend = User.UserName + ": " + txtMessage.Text;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            txtMessage.Text = "";
         }
 
 
         private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
         {
+            while (client.Connected)
+            {
+                try
+                {
+                    receive = STR.ReadLine();
+                    this.listTextMessages.Invoke(new MethodInvoker(delegate ()
+                    {
+                        listTextMessages.AppendText(receive + "\n");
+                    }));
+                    receive = "";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
         }
 
         private void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
